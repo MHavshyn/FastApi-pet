e83fb0ac93aa5ccf6a0cbda4f71f7abafcf94cc2
Havshyn Maksym <maksgavshin@gmail.com>
2025-12-05 20:10:28 +0200

    init tokens

--- backend/app/apps/auth/router.py
+++ backend/app/apps/auth/router.py
@@ -1,6 +1,7 @@
+from apps.auth.auth_handler import auth_handler
+from apps.auth.schemas import LoginResponseSchema
 from apps.core.dependencies import get_async_session
-from apps.users.crud import User, user_manager
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,12 +12,17 @@ router_auth = APIRouter()
 async def user_loging(
     data: OAuth2PasswordRequestForm = Depends(),
     session: AsyncSession = Depends(get_async_session),
-):
-    user = await user_manager.get(
-        session=session, field=User.email, field_value=data.username
+) -> LoginResponseSchema:
+    # user = await user_manager.get(
+    #     session=session, field=User.email, field_value=data.username
+    # )
+    # if not user:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+    #     )
+
+    login_response: LoginResponseSchema = await auth_handler.get_login_token_pairs(
+        session=session, data=data
     )
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    return {"data": [data.password, data.username]}
+
+    return login_response
